d2d00f334a34b8bd23f6b14c8177816c6ae1c934
Denis <Deniss2007@gmail.com>
2021-09-14 16:16:00 +0300

    todo list localStorage

---
+++ js_course/lesson27_localStorage/task3/src/scripts/index.js
@@ -0,0 +1,16 @@
+import { initTodoListHandlers } from './todoList.js';
+import { renderTasks } from './renderer.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderTasks();
+  initTodoListHandlers();
+  // renderTasks();
+});
+
+const onStorageChange = (e) => {
+  if (e.key === 'tasksList') {
+    renderTasks();
+  }
+};
+
+window.addEventListener('storage', onStorageChange);
