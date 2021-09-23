18d18882adf1f68915cfe96aa23fc7f5f42e43a5
Denis <Deniss2007@gmail.com>
2021-09-23 10:02:12 +0300

    lesson 35 errors handling & https requests

---
+++ js_course/lesson35/task5/index.js
@@ -0,0 +1,36 @@
+import { fetchUserData, fetchReposes } from './gateways.js';
+import { renderUserData } from './user.js';
+import { renderReposNames, cleanReposList } from './repos.js';
+import { showSpinner, hideSpinner } from './spinner.js';
+
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
+
+const defaultUser = {
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+  name: '',
+  location: '',
+};
+
+renderUserData(defaultUser);
+
+const onSearchUser = () => {
+  showSpinner();
+  cleanReposList();
+  const userName = userNameInputElem.value;
+  fetchUserData(userName)
+    .then((userData) => {
+      renderUserData(userData);
+      return userData.repos_url;
+    })
+    .then((url) => fetchReposes(url))
+    .then((reposList) => {
+      renderReposNames(reposList);
+    })
+    .catch((error) => {
+      alert(error.message);
+    })
+    .finally(() => hideSpinner());
+};
+
+showUserBtnElem.addEventListener('click', onSearchUser);
